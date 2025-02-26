adbbf29f36e0b436649e6909153f251e8514ffb3
mattiaferra <mattiaferrax@gmail.com>
2025-02-26 15:55:09 +0100

    aggiorngamento esercizio 4

---
+++ lezione_3/diagrammi /Esercizio 3C-4.py
@@ -0,0 +1,24 @@
+
+nome_animale = input("inserisci il nome di un animale :")
+
+match nome_animale:
+
+    case "cane"|"gatto"|"cavallo"|"elefante"|"leone":
+        
+        print(f"l'animale {nome_animale} appartiene alla categoria dei Mammiferi ")
+
+    case "serpente"|"lucertola"|"tartaruga"|"coccodrillo":
+
+        print(f"l'animale {nome_animale} appartiene alla categoria dei Rettili")
+
+    case "aquila"|"pappagallo"|"gufo"|"falco":
+
+        print(f"l'animale {nome_animale} appartiene alla categoria dei Uccelli")
+
+    case "squalo"|"trota"|"salmone"|"carpa":
+
+        print(f"l'animale {nome_animale} appartiene alla categoria dei Pesci")
+
+    case _ :
+
+       print(f"Non so dire in quale categoria classificare l'animale {nome_animale}!")
