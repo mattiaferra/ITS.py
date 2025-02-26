4300c87258851bc5bff8fb8390e264d57384f442
mattiaferra <mattiaferrax@gmail.com>
2025-02-26 16:12:52 +0100

    aggiornato

--- lezione_3/diagrammi /Esercizio 3C-4.py
+++ lezione_3/diagrammi /Esercizio 3C-4.py
@@ -1,21 +1,28 @@
 
 nome_animale = str(input("inserisci il nome di un animale:"))
 
+
+Mammiferi = ["cane", "gatto", "cavallo", "elefante", "leone"]
+Rettili = ["serpente", "lucertola", "tartaruga", "coccodrillo"]
+Uccelli =  ["aquila", "pappagallo", "gufo", "falco"]
+Pesci = ["squalo", "trota", "salmone", "carpa"]
+
+
 match nome_animale:
 
-    case "cane"|"gatto"|"cavallo"|"elefante"|"leone":
+    case nome_animale if nome_animale in Mammiferi:
         
         print(f"l'animale {nome_animale} appartiene alla categoria dei Mammiferi ")
 
-    case "serpente"|"lucertola"|"tartaruga"|"coccodrillo":
+    case nome_animale if nome_animale in Rettili:
 
         print(f"l'animale {nome_animale} appartiene alla categoria dei Rettili")
 
-    case "aquila"|"pappagallo"|"gufo"|"falco":
+    case nome_animale if nome_animale in Uccelli:
 
         print(f"l'animale {nome_animale} appartiene alla categoria dei Uccelli")
 
-    case "squalo"|"trota"|"salmone"|"carpa":
+    case nome_animale if nome_animale in Pesci:
 
         print(f"l'animale {nome_animale} appartiene alla categoria dei Pesci")
 
