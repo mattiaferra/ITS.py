2c3126ed816a9c397e9075809ee7d5fec651393a
mattiaferra <mattiaferrax@gmail.com>
2025-02-27 16:15:13 +0100

    update

---
+++ lezione_3/matchstatement/Esercizio 3C-8.py
@@ -0,0 +1,24 @@
+frase = input("inserisci una frase (deve terminare con '?' o '!' oppure senza niente.)")
+
+
+match frase :
+
+    case frase if frase[len(frase)-1]=="?" and len(frase)%2==0:
+
+        print("SI")
+
+    case frase if frase[len(frase)-1]=="?" and len(frase)%2!=0:
+
+        print("NO")
+
+    case frase if frase[len(frase)-1]=="!":
+        
+        print("WOW")
+
+    case _ :
+
+        print(f"Tu dici {frase}")
+        
+
+
+
