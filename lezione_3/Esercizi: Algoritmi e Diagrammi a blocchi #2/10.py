be711f8e11e93eb14cf7ad6837518f147458dea7
mattiaferra <mattiaferrax@gmail.com>
2025-03-12 19:15:50 +0100

    add 10

---
+++ lezione_3/Esercizi: Algoritmi e Diagrammi a blocchi #2/10.py
@@ -0,0 +1,16 @@
+eta = int(input("inserisci l'eta : "))
+
+
+if eta >= 18 and eta <= 65 :
+
+    print("puoi partecipare all' attività ")
+
+
+elif eta < 18 :
+
+    print("non puoi partecipare all' attività perchè non hai raggiunto l'età minima richiesta ")
+
+
+else : 
+
+    print("non puoi partecipare all' attività perchè hai superato l'età massima richiesta ")
