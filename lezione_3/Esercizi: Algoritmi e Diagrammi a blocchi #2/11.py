72d62027967cdc04fcc53fa29db2811489351e3f
mattiaferra <mattiaferrax@gmail.com>
2025-03-13 15:23:55 +0100

    add 11

---
+++ lezione_3/Esercizi: Algoritmi e Diagrammi a blocchi #2/11.py
@@ -0,0 +1,24 @@
+
+
+
+while True :
+
+    n = int(input("inserisci n : "))
+
+    if n % 1 == 0 :
+
+        if n % 2 == 0 and n > 10 :
+
+            print("numero valido ")
+            break
+
+        else : 
+
+            print("numero non valido ")
+
+
+
+
+         
+
+
