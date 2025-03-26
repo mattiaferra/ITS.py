79010df6abae45e4aba7acdaedb27328d447b5e7
mattiaferra <mattiaferrax@gmail.com>
2025-03-26 19:23:48 +0100

    da finire

---
+++ lezione_6/lezione_6_obbligatori/9-3.py
@@ -0,0 +1,12 @@
+class User : 
+
+    def __init__(self,first_name,last_name):
+
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def describe_user(self):
+
+        print(f"Nome : {self.first_name}/nCognome : {self.last_name}")
+
+        
