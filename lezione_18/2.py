a74f857ace2599e5c3b1f35994ac62f3c03d406b
mattiaferra <mattiaferrax@gmail.com>
2025-04-03 14:03:25 +0200

    save

--- lezione_18/2.py
+++ lezione_18/2.py
@@ -1,3 +1,6 @@
+import string
+
+
 def validate_password(password):
 
     somma_carattere_maiusc = 0
@@ -8,12 +11,30 @@ def validate_password(password):
             somma_carattere_maiusc += 1
 
 
-    if len(password) <= 20 or somma_carattere_maiusc <= 3:
-        raise Exception("la password deve essere lunga almeno 20 caratteri e deve avere almeno 3 lettere maiuscole")
+    cont_cs = 0
+
+    for i in password:
+
+        if i in string.punctuation:
+            carattere_speciale = i
+            cont_cs += 1
+
+    if len(password) < 20 or somma_carattere_maiusc < 3 or cont_cs < 4:
+        raise ValueError("la password deve essere lunga almeno 20 caratteri e deve avere almeno 3 lettere maiuscole")
     
 
+    else : 
+        print("password corretta")
     
-            
+try :
+    validate_password("Ciao!!")
+
+except ValueError as e:
+    print(e)
+
     
-    
+
+
+
+
     
