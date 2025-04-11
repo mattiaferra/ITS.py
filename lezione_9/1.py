957281fa3e2ab94950a30b7bc80f3c5d278bf0f5
mattiaferra <mattiaferrax@gmail.com>
2025-04-11 17:18:18 +0200

    update dom ese

--- lezione_9/1.py
+++ lezione_9/1.py
@@ -6,6 +6,9 @@ Esercizio 1.1: Scrivi una RegEx che riconosca un numero intero positivo (es. 123
 Esercizio 1.2: Modifica la RegEx per accettare anche numeri negativi (es. -45, -1000, 0). = ^-?\d+$
 
 
+ 
+
+
 
 
 
