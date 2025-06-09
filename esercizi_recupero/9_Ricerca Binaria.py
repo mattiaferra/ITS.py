b5c850e831942a0996c66ae9fd258d1a77c2cfef
mattiaferra <mattiaferrax@gmail.com>
2025-06-10 00:27:01 +0200

    Add files via upload
    commit 9

---
+++ esercizi_recupero/9_Ricerca Binaria.py
@@ -0,0 +1,39 @@
+def ricerca_binaria(num_cercato, lista):
+
+
+     lista.sort()  # Assicurati che la lista sia ordinata
+
+     sinistra =0
+     destra = len(lista)-1
+
+
+     while sinistra <= destra : # finchè sinistra è minore uguale a destra significa che devi ancora controllare altri elementi
+          
+          medio = (sinistra+destra)//2
+
+          if num_cercato == lista[medio]: # se è nel punto medio l'hai trovato
+               return True
+          
+          elif num_cercato < lista[medio]:
+                                                    #se il numero cercato è piu piccolo del numero che sta nel punto medio essendo una lista ordinata in modo crescente
+              destra = medio -1
+          
+
+          else:
+               sinistra = medio + 1
+
+
+     return False
+               
+
+       
+
+
+
+    
+           
+           
+
+        
+
+
