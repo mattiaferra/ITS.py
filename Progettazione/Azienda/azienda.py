f6219c07edfb5dc3f45f60c0e11437dd8422ef4e
mattiaferra <mattiaferrax@gmail.com>
2025-06-19 17:16:58 +0200

    commit azienda

--- Progettazione/Azienda/azienda.py
+++ Progettazione/Azienda/azienda.py
@@ -1,31 +1,48 @@
+from __future__ import annotations
 from mytypes import*
+
+class Coinvolgimento : 
+
+    def __init__(self, impiegato : Impiegato , progetto : Progetto , data_coinvolgimento : date):
+        self.impiegato = impiegato
+        self.progetto = progetto
+        self.data_coinvolgimento = data_coinvolgimento
 
 
 class Impiegato :
+    _nome: str
+    ...
+    _coinvolgimenti: set[Coinvolgimento] # tutti i link dell'impiegato verso i progetti in cui è coinvolto
     
     def __init__(self , nome : str , cognome : str , nascita : date , stipendio : RealGez):
-        
-        self.nome = nome 
-        self.cognome = cognome
+        self._nome = nome 
+        self._cognome = cognome
         self.nascita = nascita
         self.stipendio = stipendio
+        self._coinvolgimenti: set[Coinvolgimento] = set()
 
+    def aggiungi_progetto(self, progetto : "Progetto" , data : date):
+        coinvolgimento = Coinvolgimento(self, progetto , data)
+        self._coinvolgimenti.add(coinvolgimento)
+        progetto._coinvolgimenti.add(coinvolgimento)
+
+    def rimuovi_progetto(self, progetto : "Progetto" , data : date):
+        coinvolgimento = Coinvolgimento(self, progetto, date.today())
+        self._coinvolgimenti.discard(coinvolgimento)
+        progetto._coinvolgimenti.discard(coinvolgimento)
 
 
 class Progetto :
     _nome: str # noto alla nascita
-    _impiegati_coinvolti: dict[Impiegato, date] # certamente non noto alla nascita
+    _coinvolgimenti: set[Coinvolgimento] # certamente non noto alla nascita
     
     def __init__(self , nome : str , budget : RealGez):
-        
         self._nome = nome
         self._budget = budget
-        self._impiegati_coinvolti = dict()
-
-
+        self._coinvolgimenti : set[Coinvolgimento] = set()
 
     def get_nome(self) -> str:
-         return self._nome
+        return self._nome
 
     def get_budget(self) -> RealGez:
         return self._budget
@@ -36,59 +53,36 @@ class Progetto :
     def set_budget(self, budget: RealGez):
         self._budget = budget
 
-
     def add_impiegato(self ,impiegato: Impiegato, data : date):
-        self._impiegati_coinvolti[impiegato] = data
-
-        # return self.impiegati_coinvolti
-    
+        coinvolgimento = Coinvolgimento(impiegato,self,data)
+        impiegato._coinvolgimenti.add(coinvolgimento)
+        impiegato._coinvolgimenti.add(coinvolgimento)
 
     def is_coinvolto(self, impiegato : Impiegato) -> bool:
-
-        return impiegato in self._impiegati_coinvolti
-            
+        return impiegato in self._coinvolgimenti
 
     def ultimo_impiegato_coinvolto(self):
-
-        if not self._impiegati_coinvolti:
+        if not self._coinvolgimenti:
             return None
-
 
         ultimo_impiegato = None
         ultima_data = None
-
-
-        for impiegato,data in self._impiegati_coinvolti.items():
-
+        for impiegato,data in self._coinvolgimenti.items():
             if ultima_data is None  or data > ultima_data:
                 ultima_data = data
                 ultimo_impiegato = impiegato
                 
         return ultimo_impiegato
-    
 
     def remove_impiegato(self, impiegato : Impiegato):
-
-        if impiegato in self._impiegati_coinvolti:
-              del self._impiegati_coinvolti[impiegato]
-
-
-
-
-
-
-            
-
-
-        
-
-
+        coinvolgimento = Coinvolgimento(impiegato, self, date.today())
+        self._coinvolgimenti.discard(coinvolgimento)
+        impiegato._coinvolgimenti.discard(coinvolgimento)
 
 
 class Dipartimento : 
 
     def __init__(self , nome : str , telefono : NumeroTelefono , indirizzo : Indirizzo):
-        
         self.nome = nome
         self.telefono = telefono
         self.indirizzo = indirizzo
